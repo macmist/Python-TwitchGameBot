36d6134998f6e1b1228717f2a59a81a08a54c023
Quentin MUSY <musy_q@epita.fr>
2017-04-03 22:37:24 +0200

    [CONFIG] default config file

---
+++ cfg.py
@@ -0,0 +1,5 @@
+HOST = "irc.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = "twitch_username"            # your Twitch username, lowercase
+PASS = "oauth:xxxxxxxxxxxxxxxxxxxx" # your Twitch OAuth token
+CHAN = "#channel"                   # the channel you want to join
